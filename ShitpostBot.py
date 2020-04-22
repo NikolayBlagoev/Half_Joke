907d4a9ad8e9c23c1faafaabce9afd8a3cdc8072
NikolayBlagoev <62670626+NikolayBlagoev@users.noreply.github.com>
2020-04-22 17:13:57 +0300

    Improvements to Text adding

--- ShitpostBot.py
+++ ShitpostBot.py
@@ -1,7 +1,7 @@
 from random import randrange
 from time import sleep
 from PIL import Image, ImageDraw, ImageFilter, ImageFont, ImageEnhance
-
+import math
 # Completely ruins an image
 def deepfry(img):
     img = img.filter(ImageFilter.SHARPEN)
@@ -15,55 +15,77 @@ def deepfry(img):
     img = img.filter(ImageFilter.SHARPEN)
     img = img.filter(ImageFilter.SHARPEN)
     return img
+def splitString(input):
+    input=input.replace("\n"," ")
+    output=[]
+    const=30*max(1,int(len(input)/180))
+    start=0
+    end=const
+    while end<len(input):
+        if (input[end]!=" " or input[end]!="\n" or input[end]!="\0") :
+            i=end
+            p=end
+            steps=0
+            while steps<10:
+                if i<len(input)and (input[i]==" " or input[i]=="\n" or input[i]=="\0"):
+                    end=i
+                    break
+                if p>=0 and ( input[p] == " " or input[p] == "\n" or input[p] == "\0"):
+                    end = p
+                    break
+                steps+=1
+                i+=1
+                p-=1
+
+        output.append(input[start:end])
+        start=end
+        end+=const
+    if start<len(input):
+        output.append(input[start:])
+    return output
+def drawTextLowerBorder(draw,img, text,bottomy,magnify=1):
+    text = splitString(text)
+    lines = len(text)
+    fontSize = int((img.height / 10) * magnify / max(math.ceil(lines / 2)-1,1))
+    for i in range(len(text)):
+        txt = text[i]
+        font = ImageFont.truetype("impact.ttf", fontSize)
+        (w, h) = draw.textsize(txt, font=font)
+        x = int((img.width - w) / 2)
+        y = bottomy - ( (len(text) - i) * h)
+        draw.text((x - 2, y + 2), txt, font=font, fill="black")
+        draw.text((x + 2, y + 2), txt, font=font, fill="black")
+        draw.text((x - 2, y - 2), txt, font=font, fill="black")
+        draw.text((x + 2, y - 2), txt, font=font, fill="black")
+        draw.text((x, y), txt, (255, 255, 255), font=font)
+def drawTextUpperBorder(draw,img, text,top,magnify=1):
+    text = splitString(text)
+    lines = len(text)
+    fontSize = int((img.height / 10) * magnify / max(math.ceil(lines / 2)-1,1))
+    for i in range(len(text)):
+        txt = text[i]
+        font = ImageFont.truetype("impact.ttf", fontSize)
+        (w, h) = draw.textsize(txt, font=font)
+        x = int((img.width - w) / 2)
+        y = top + (i * h)
+        draw.text((x - 2, y + 2), txt, font=font, fill="black")
+        draw.text((x + 2, y + 2), txt, font=font, fill="black")
+        draw.text((x - 2, y - 2), txt, font=font, fill="black")
+        draw.text((x + 2, y - 2), txt, font=font, fill="black")
+        draw.text((x, y), txt, (255, 255, 255), font=font)
 
 # Adds toptext and bottom text
 def addText(img,topText="",bottomText="BOTTOMTEXT",magnify=1):
     draw = ImageDraw.Draw(img)
-    fontSize = int(img.height / 10)*magnify
-    font = ImageFont.truetype("impact.ttf", fontSize)
-    x = (img.width / 2 - len(topText) * int(fontSize / 4))
-    y = 5
-    draw.text((x - 2, y + 2), topText, font=font, fill="black")
-    draw.text((x + 2, y + 2), topText, font=font, fill="black")
-    draw.text((x - 2, y - 2), topText, font=font, fill="black")
-    draw.text((x + 2, y - 2), topText, font=font, fill="black")
-    draw.text((x, y), topText, (255, 255, 255), font=font)
-    lines=bottomText.count("\n")+1
-
-    fontSize =int( (img.height / 10 )* magnify/lines)
-    font = ImageFont.truetype("impact.ttf", fontSize)
-    x = (img.width / 2 - len(bottomText) * int(fontSize / 4)/2)
-    y = img.height - 100*magnify*lines
-    draw.text((x - 2, y + 2), bottomText, font=font, fill="black")
-    draw.text((x + 2, y + 2), bottomText, font=font, fill="black")
-    draw.text((x - 2, y - 2), bottomText, font=font, fill="black")
-    draw.text((x + 2, y - 2), bottomText, font=font, fill="black")
-    draw.text((x, y), bottomText, (255, 255, 255), font=font)
+    drawTextUpperBorder(draw,img,topText,5)
+    drawTextLowerBorder(draw,img,bottomText,img.height-10)
     return img
 
 # Combines template and source
 
-path ='-'
-img = Image.open(path+"-",'r')
-
-img2 = Image.open(path+"-")
-yRatio=1200/img2.height
-xRatio=650/img2.width
-ration=max(xRatio,yRatio)
-img2=img2.resize((int(img2.width*ration), int(img2.height*ration)))
-
-background = Image.new('RGBA', (img.width, img.height), (0, 255, 255, 255))
-background.paste(img2, (int(-img2.width/2+300), int(-img2.height/2+700)))
-
-background.paste(img, (0, 0), mask=img)
-
-img=background
-img=deepfry(img)
-img.save(path+'sample-out.png')
-img.show();
-sleep(3)
-img = Image.open(path+"-",'r')
-img=addText(img,"", "",1)
+path =''
+img = Image.open(path+"",'r')
+img=addText(img,"")
 
 img.save(path+'sample-out.png')
 img.show();
